95931063dd5b7b79edf97d0a161c3e33819d9512
thapapratyush <thapa.pratyush10@gmail.com>
2017-11-21 13:23:11 -0500

    Updated front end

--- Compilo frontend/app.js
+++ Compilo frontend/app.js
@@ -14,13 +14,39 @@
     const btnLogin = document.getElementById('btnLogin');
     const btnSignUp = document.getElementById('btnSignup');
     const btnLogout = document.getElementById('btnLogout');
+    // const uploadBtn = document.getElementById('uploadBtn');
+    const containerHTML = "<form>\
+                <div class=\"group\">  \
+                    <input id=\"txtEmail\" required>\
+                    <span class=\"highlight\"></span>\
+                    <span class=\"bar\"></span>\
+                    <label>Email</label>\
+                </div>\
+                <div class=\"group\">\
+                    <input id=\"txtPassword\" type=\"password\" required>\
+                    <span class=\"highlight\"></span>\
+                    <span class=\"bar\"></span>\
+                    <label>Password</label>\
+                </div>\
+            </form>\
+            <button id=\"btnLogin\" class=\"btn btn-primary\">\
+                Log in\
+            </button>\
+            <button id=\"btnSignup\" class=\"btn btn-outline-secondary\">\
+               Sign up\
+            </button>\
+            <div id=\"errorMessage\"></div>"
+
+    // uploadBtn.addEventListener('click', e=>{
+    //     document.getElementById("codeForm");
+    // })
 
     btnLogin.addEventListener('click', e => {
         const email = txtEmail.value;
         const pass = txtPassword.value;
         const auth = firebase.auth();
         const promise = auth.signInWithEmailAndPassword(email, pass);
-        promise.catch(e=>console.log(e.message));
+        promise.catch(e=>console.log(e));
     });
 
     btnSignUp.addEventListener('click', e=> {
@@ -37,16 +63,34 @@
     
     btnLogout.addEventListener('click', e=>{
         firebase.auth().signOut();
+        document.getElementById('container').innerHTML= containerHTML;
     });
 
     firebase.auth().onAuthStateChanged(firebaseUser => {
         if(firebaseUser) {
             console.log(firebaseUser);
             btnLogout.classList.remove('invisible');
+            personName.classList.remove('invisible');
+            var email = txtEmail.value;
+            email = email.replace('@bison.howard.edu', '');
+            email = email.replace('.', ' ');
+            var userName = namify(email);
+            document.getElementById('personName').innerHTML = userName;
+            document.getElementById('container').innerHTML = null;
+            document.getElementById('upload').classList.remove('invisible');
         }
         else {
+            console.log(containerHTML);
             console.log('not logged in');
             btnLogout.classList.add('invisible');
+            document.getElementById('upload').classList.add('invisible');
+            document.getElementById('personName').innerHTML = null;
         }
     })
+
+    function namify(str) {
+    return str.replace(/\w\S*/g, function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
 }())
