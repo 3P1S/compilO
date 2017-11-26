005b445d48c1456e7c9a27cda42822f02b4a849d
PBhandari99 <prashantb1097@gmail.com>
2017-11-26 18:18:32 -0500

    the backend service is done

--- flask_compilo/app.py
+++ flask_compilo/app.py
@@ -6,9 +6,10 @@ from flask import Flask, request, redirect, jsonify, make_response
 from werkzeug.utils import secure_filename
 
 
-UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = set(['cpp', 'cc'])
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = set(["cpp", "cc"])
 COMPILER_PATH = "/usr/bin/g++"
+DB_TABLE = "table1"
 
 app = Flask("compilO")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -36,7 +37,6 @@ def submit_file(username):
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect(request.url)
-        userID = username
         file = request.files['file']
         if file.filename == '':
             return redirect(request.url)
@@ -44,19 +44,39 @@ def submit_file(username):
             filename = secure_filename(file.filename)
             print ("Filename is: {0}".format(filename))
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            compilation_output = compile_cpp(app.config["UPLOAD_FOLDER"]+"/"+filename)
+            compilation_file = app.config["UPLOAD_FOLDER"]+"/"+filename
+            compilation_output = compile_cpp(username, compilation_file)
             print (compilation_output)
             return make_response(jsonify(result=compilation_output))
-
-        return make_response(jsonify(status="uploaded"))
-
-#  def unzip_tar_file(file_to_unzip):
-    #  tar = tarfile.open(file_to_unzip)
-    #  tar.extractall('./uploads/extracts')
-    #  tar.close()
+        return make_response(jsonify(status="upload failed"))
 
 
-def compile_cpp(file_name):
+@app.route('/compilations', methods=['GET'])
+def get_compilation_results():
+    results_db = get_all_results(DB_TABLE)
+    print (results_db)
+    response = {}
+    if results_db:
+        for i in results_db:
+            for j in range(2, len(i)):
+                if i[j] != None:
+                    response[i[0]] = i[j]
+        return make_response(jsonify(status=response))
+
+
+@app.route('/compilations/<username>', methods=['GET'])
+def get_compilation_results_by_user(username):
+    results_db = get_results_by_users(DB_TABLE, username)
+    print(results_db)
+    if results_db:
+        response = {}
+        for j in range(2, len(results_db)):
+            if results_db[j] != None:
+                response[results_db[0]] = results_db[j]
+        return make_response(jsonify(status=response))
+
+
+def compile_cpp(userID, file_name):
     #  executable_name =
     process = subprocess.Popen([COMPILER_PATH, "-Wall", "-o",
                                "executable", file_name],
@@ -65,13 +85,19 @@ def compile_cpp(file_name):
     compilation_output = process.communicate()
     if compilation_output[1]:
         print("failed to compile")
+        write_file_result_to_db(userID, compilation_file, DB_TABLE,
+                compile_err=compilation_output[1])
         return compilation_output[1]
     process = subprocess.Popen(["./executable"], stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
     return_value = process.communicate()
     if return_value[1]:
         print("There was a runtime error!")
+        write_file_result_to_db(userID, compilation_file, DB_TABLE,
+                run_time_err=return_value[1])
         return return_value[1]
+    write_file_result_to_db(userID, compilation_file, DB_TABLE,
+            result=return_value[0])
     return return_value[0]
 
 
@@ -81,30 +107,37 @@ def init_db():
     return db_connect
 
 
-#  def create_table(table):
-    #  db = init_db()
-    #  db_cursor = db.cursor()
-    #  # TODO: Fix this shit, it's too long.
-    #  # I guess you can also do "?" for the tabel name, make sure you get rid of {0}.
-    #  query = "CREATE TABLE {0} (USER_ID  TEXT NOT NULL, assignment TEXT NOT NULL,".format(table) +
-            #  "file BLOB NOT NULL, compile_error text, run_time_error text, result text)"
-    #  db_cursor.execute(query)
-    #  db.commit()
-    #  db.close()
+def create_table(table, db_cursor):
+    # TODO: Fix this shit, it's too long.
+    # I guess you can also do "?" for the tabel name, make sure you get rid of {0}.
+    first_half = "CREATE TABLE {0} (USER_ID TEXT NOT NULL, ".format(table) 
+    query = first_half + "file BLOB NOT NULL, compile_error text, run_time_error text, result text)"
+    try:
+        db_cursor.execute(query)
+    except sqlite3.OperationalError as error:
+        if error.message == "table {0} already exists".format(DB_TABLE):
+            print("Table already exists.")
+        else:
+            raise error
 
-def write_file_result_to_db(user_id, assignment, file, table, compile_err=None,
+
+#  make sure to delete the file after adding it to db
+def write_file_result_to_db(user_id, file, table, compile_err=None,
                             run_time_err=None, result=None):
     db = init_db()
     db_cursor = db.cursor()
+    create_table(table, db_cursor)
+    with open(file, "rb") as f:
+        ablob = f.read()
     if compile_err:
-        data = (user_id, assignment, file, compile_err)
+        data = (user_id, buffer(ablob), compile_err, None, None)
     elif run_time_err:
-        data = (user_id, assignment, file, run_time_err)
+        data = (user_id, buffer(ablob), None, run_time_err, None)
     elif result:
-        data = (user_id, assignment, file, result)
+        data = (user_id, buffer(ablob), None, None, result)
     else:
         return -1
-    db_cursor.execute("INSERT INTO {0} VALUES(?, ?, ?, ?, ?, ?)".format(table),
+    db_cursor.execute("INSERT INTO {0} VALUES(?, ?, ?, ?, ?)".format(table),
                       data)
     db.commit()
     db.close()
@@ -117,8 +150,12 @@ def get_results_by_users(table, user_id):
     db_cursor = db.cursor()
     if user_id:
         data = (user_id,)
-        row = db_cursor.execute("SELECT * FROM {0} WHERE USER_ID=?".format(table), data).fetchone()
+        row = db_cursor.execute(
+                "SELECT * FROM {0} WHERE USER_ID=?".format(table),
+                data).fetchone()
         db.close()
+        #  with open("./uploads/{0}-{1}.cc".format(user_id, row[1])) as file_from_db:
+            #  file_from_db.write(row[2])
         return row
     db.close()
     return "No user_id"
@@ -126,7 +163,7 @@ def get_results_by_users(table, user_id):
 def get_all_results(table):
     db = init_db()
     db_cursor = db.cursor()
-    row = db_cursor.execute("SELECT * FROM {0}".format(table)).fetchone()
+    row = db_cursor.execute("SELECT * FROM {0}".format(table)).fetchall()
     db.close()
     return row
 
